c3a2a69997bcf601fdd9f14c2ec5c4aaf00f4fcb
H1B4N4 <67756516+H1B4N4@users.noreply.github.com>
2020-07-02 20:57:26 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+
+
+
+emojis = '🇴🇭'
+poehavshie = {
+724475443297779722, 460550679942135809, 583991031016456192 #zyabl
+}
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Запущено.")
+
+@client.event
+async def on_message(message):
+    if message.author.id in poehavshie:
+        for em in emojis:
+            await message.add_reaction(em)
+
+
+token = os.environ.get('BOT_TOKEN') # Получаем токен с heroku который ты указывал в настройках
+client.run(str(token)) # запускаем бота
